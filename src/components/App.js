5170bca05ecb450e6dca71653c1711d405ce6c18
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-21 18:19:53 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
+/** @format */
+
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => setDogImage(data.message));
+  });
+
+  return (
+    <div>
+      {dogImage ? (
+        <img
+          src={dogImage}
+          alt="A Random Dog"
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
